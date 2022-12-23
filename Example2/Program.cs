f152149f8d989c97b338c70456dea82881eb85dd
Kiesaro <freestylerspb@gmail.com>
2022-12-24 01:30:46 +0300

    Example 2

---
+++ Example2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA>numberB)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(numberA);
+    Console.Write("Min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(numberB);
+    Console.Write("Min = ");
+    Console.WriteLine(numberA);
+}
+
+
+
