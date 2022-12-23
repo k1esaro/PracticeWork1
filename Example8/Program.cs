04dc6e0771d521276b5698bbbcb448003003051b
Kiesaro <freestylerspb@gmail.com>
2022-12-24 01:50:33 +0300

    Example 8

---
+++ Example8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите  число N");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number%2==0)
+{
+    while (number>0)
+    {
+        Console.Write(number);
+        Console.Write(" ");
+        number= number-2;   
+    }
+}
+else
+{
+    number= number - 1;
+    while (number>0)
+    {
+        Console.Write(number);
+        Console.Write(" ");
+        number= number-2;   
+    }
+}
