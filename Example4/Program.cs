9f2341b5fb5ece1069d1caea47db6d016df3eb44
Kiesaro <freestylerspb@gmail.com>
2022-12-24 01:36:32 +0300

    Example4.2

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberС = Convert.ToInt32(Console.ReadLine());
+int max = 0 ;
+if(numberA>numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+if (max>numberС)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+}
+else
+{
+    max = numberС;
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+}
